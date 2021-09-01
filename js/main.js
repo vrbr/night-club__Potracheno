7a41cb81cd2da1b8b949581b712c2446e3b1ae44
Мария <lpvamaria@gmail.com>
2021-09-01 20:53:31 +0300

    Оформлено меню с напитками

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 const menu = document.querySelector('.menu__popup');
 const menuBtn = document.querySelector('.menu__btn');
 
+const tab = document.querySelectorAll('.tab');
+
+const foodMenu = document.querySelector('.food-menu');
+const drinkMenu = document.querySelector('.drink-menu');
+const filter1 = document.querySelector('.filter-1');
+const filter2 = document.querySelector('.filter-2');
+
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('is-active');
+});
+
+tab.forEach(item => {
+    item.addEventListener('click', (e) => {
+        event.preventDefault();
+        tab.forEach(el => {
+            el.classList.remove('tab--active');
+        });
+        item.classList.add('tab--active');
+    });
+});
+
+filter1.addEventListener('click', function() {
+    foodMenu.classList.add('menu--active');
+    drinkMenu.classList.remove('menu--active');
+});
+filter2.addEventListener('click', function() {
+    drinkMenu.classList.add('menu--active');
+    foodMenu.classList.remove('menu--active');
 });
 
 //swiper
