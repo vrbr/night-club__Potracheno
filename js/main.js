a6a8e4706d9c85d95cc2c7ef8a46cebc9303b8b1
Мария <lpvamaria@gmail.com>
2021-09-04 18:47:05 +0300

    Модальное окно карточки меню

--- js/main.js
+++ js/main.js
@@ -3,10 +3,17 @@ const menuBtn = document.querySelector('.menu__btn');
 
 const tab = document.querySelectorAll('.tab');
 
+const cartDetail = document.getElementById('#tiger-pramn-salad');
+const cartDetailPopup = document.getElementById('#tiger-pramn-salad__popup');
+const cartDetailClose = cartDetailPopup.querySelector('.button-close');
+const body = document.querySelector('body');
+
+//burger
 menuBtn.addEventListener('click', () => {
     menu.classList.toggle('is-active');
 });
 
+//menu-main - tabs
 tab.forEach(item => {
     item.addEventListener('click', (e) => {
         event.preventDefault();
@@ -15,4 +22,13 @@ tab.forEach(item => {
         });
         item.classList.add('tab--active');
     });
+});
+
+//food cart
+cartDetail.addEventListener('click', () => {
+    cartDetailPopup.classList.add('open-card');
+    body.style.overflow = 'hidden';
+});
+cartDetailClose.addEventListener('click', () => {
+    cartDetailPopup.classList.remove('open-card');
 });
